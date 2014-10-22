a6db9fdf151e9e96af76bba6dc9de584944585de
Tregubov Vadim <v.tregubov@rambler-co.ru>
2014-10-22 09:32:34 +0400

    Fixed User admin

--- app/accounts/admin.py
+++ app/accounts/admin.py
@@ -1,9 +1,29 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from django import forms
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm
 from .models import *
 
 
-class UserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserCreationForm(UserCreationForm):
 
-admin.site.register(User, UserAdmin)
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        try:
+            self.Meta.model.objects.get(username=username)
+        except self.Meta.model.DoesNotExist:
+            return username
+        raise forms.ValidationError(
+            self.error_messages['duplicate_username'],
+            code='duplicate_username',
+        )
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+
+admin.site.register(User, CustomUserAdmin)
